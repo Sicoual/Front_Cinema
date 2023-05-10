015d122a8376ca1bf1b6a8cb56e401ff5b492eb4
marine <marine.barbarinwalker@gmail.com>
2023-05-10 11:32:05 +0200

    dev Marine

--- Cinema/src/app/app.module.ts
+++ Cinema/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfosFilmComponent } from './infos-film/infos-film.component';
+import { FilmService } from './film.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfosFilmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FilmService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
