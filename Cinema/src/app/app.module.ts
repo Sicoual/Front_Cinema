b5f2e1ed91035b7cab31890841ef5236385422a9
Jaufret <jaufret.marg@hotmail.com>
2023-05-11 08:41:51 +0200

    Commit Jaufret Header

--- Cinema/src/app/app.module.ts
+++ Cinema/src/app/app.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarrouseldebaseComponent } from './carrouseldebase/carrouseldebase.component';
 import { PageDeImage1Component } from './page-de-image1/page-de-image1.component';
 import { InfosFilmComponent } from './infos-film/infos-film.component';
 import { FilmService } from './film.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarrouseldebaseComponent,
-  InfosFilmComponent,
+    InfosFilmComponent,
+    HeaderComponent,
     PageDeImage1Component
     ],
   
@@ -22,7 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
      HttpClientModule,
-    NgbModule   
+     FormsModule,
+      
   ],
  
   providers: [FilmService],
