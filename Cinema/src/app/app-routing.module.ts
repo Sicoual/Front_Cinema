0e2c138b5d07f5ea4e6cba851a1b3239d39087d2
Xczema <vincent.tang06@gmail.com>
2023-05-12 10:47:21 +0200

    ajout du composant grille-films qui permet d'afficher tous les films

--- Cinema/src/app/app-routing.module.ts
+++ Cinema/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageDeImage1Component } from './page-de-image1/page-de-image1.component';
 import { InfosFilmComponent } from './infos-film/infos-film.component';
+import { GrilleFilmsComponent } from './voirTousLesFilms/grille-films/grille-films.component';
 
 const routes: Routes = [
-  {path:"pageDeImage1", component:PageDeImage1Component },
- {path:"infos", component:InfosFilmComponent}  
+  { path: "infos", component: InfosFilmComponent },
+  { path: "filmographie", component: GrilleFilmsComponent },
 ];
 
 @NgModule({
